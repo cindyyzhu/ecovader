445f20aca2eff397f296d7422826c4d23aa908c5
cindyyzhu <cindyzhu8207@gmail.com>
2024-02-17 13:09:15 -0500

    added pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Dashboard from "./Dashboard";
+import Regionselect from "./Regionselect";
+import Community from "./Community";
 
 function App() {
   return (
     <Routes>
         <Route exact path="/" element={<Dashboard />}> </Route>
+        <Route exact path="/regionselect" element={<Regionselect />}> </Route>
+        <Route exact path="/community" element={<Community />}> </Route>
     </Routes>
   );
 }
