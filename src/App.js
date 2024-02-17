5f6e9171abb5df2d26ec9ffee1bfb0fa8d3f6bda
cindyyzhu <cindyzhu8207@gmail.com>
2024-02-17 17:19:34 -0500

    added file locations

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Regionselect from "./pages/Regionselect";
+import Community from "./pages/Community";
+
+function App() {
+  return (
+    <Routes>
+        <Route exact path="/" element={<Dashboard />}> </Route>
+        <Route exact path="https://www.ecovader.biz/regionselect" element={<Regionselect />}> </Route>
+        <Route exact path="https://www.ecovader.biz/community" element={<Community />}> </Route>
+    </Routes>
+  );
+}
+
+export default App;
